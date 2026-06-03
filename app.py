5dc3a93c29472b7f3a2a5b93c4261c3cbac99886
marcelovh75098 <marcelovh7509@gmail.com>
2026-06-03 16:47:32 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Lottery Analytics",
+    layout="wide"
+)
+
+st.title("Lottery Analytics Dashboard")
+
+st.success("Primer despliegue exitoso 🚀")
