1bf0bfc0ae8904834e6ad20d51855bc04ee9b874
marcelovh75098 <marcelovh7509@gmail.com>
2026-06-05 17:12:55 -0500

    Mejorar prueba scraper

--- scrapers/baloto_scraper.py
+++ scrapers/baloto_scraper.py
@@ -5,19 +5,14 @@ URL = "https://www.baloto.com/filtro-historico-de-resultados"
 
 def test_connection():
 
-    try:
+    response = requests.get(URL, timeout=30)
 
-        response = requests.get(URL, timeout=30)
+    print("STATUS:", response.status_code)
 
-        print("Status Code:", response.status_code)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-        if response.status_code == 200:
-            print("Conexion exitosa con Baloto")
-        else:
-            print("Error al conectar")
-
-    except Exception as e:
-        print("Error:", e)
+    print("TITULO PAGINA:")
+    print(soup.title.text if soup.title else "Sin titulo")
 
 if __name__ == "__main__":
     test_connection()
