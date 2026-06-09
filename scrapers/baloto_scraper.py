5ffeebed3d0877e95635779e5c11310bbbf16c46
marcelovh75098 <marcelovh7509@gmail.com>
2026-06-09 16:59:37 -0500

    Update baloto_scraper.py

--- scrapers/baloto_scraper.py
+++ scrapers/baloto_scraper.py
@@ -30,14 +30,32 @@ def obtener_ultimo_sorteo():
 
         filas = tabla.find_all("tr")
 
-        if len(filas) > 1:
-
+        if len(filas) <= 1:
             return {
-                "html_primer_sorteo": str(filas[1])
+                "error": "No se encontraron sorteos"
             }
 
+        fila = filas[1]
+
+        columnas = fila.find_all("td")
+
+        fecha = columnas[1].get_text(strip=True)
+
+        resultado_texto = columnas[2].get_text(
+            separator=" ",
+            strip=True
+        )
+
+        numeros = resultado_texto.replace("-", " ").split()
+
         return {
-            "error": "No se encontraron sorteos"
+            "fecha": fecha,
+            "n1": numeros[0],
+            "n2": numeros[1],
+            "n3": numeros[2],
+            "n4": numeros[3],
+            "n5": numeros[4],
+            "superbalota": numeros[5]
         }
 
     except Exception as e:
