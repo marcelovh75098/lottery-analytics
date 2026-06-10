5f69cf1948ed29ed64b595d24a775ce558ef9c5e
marcelovh75098 <marcelovh7509@gmail.com>
2026-06-09 20:58:27 -0500

    Update backtester.py

--- engine/backtester.py
+++ engine/backtester.py
@@ -1,7 +1,7 @@
 from engine.features import build_features
 
 
-def evaluate_prediction(pred, actual):
+def evaluate(pred, actual):
     return len(set(pred) & set(actual[:5]))
 
 
@@ -16,17 +16,13 @@ def backtest(strategies, draws):
         for i in range(len(draws) - 1):
 
             history = draws[:i+1]
-
             actual = draws[i+1]
 
-            # 🔥 AQUÍ ESTÁ EL FIX CRÍTICO
             features = build_features(history)
 
             pred = s.predict(features)
 
-            score = evaluate_prediction(pred, actual)
-
-            scores.append(score)
+            scores.append(evaluate(pred, actual))
 
         results[s.name()] = scores
 
