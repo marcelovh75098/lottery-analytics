5d8ce301f26dec8b44283a73ae796c04eec1a155
marcelovh75098 <marcelovh7509@gmail.com>
2026-06-09 19:54:58 -0500

    Update frequency.py

--- strategies/frequency.py
+++ strategies/frequency.py
@@ -1,26 +1,11 @@
-from collections import Counter  # Permite contar frecuencia de números
-
-
 class FrequencyStrategy:
-    # Estrategia basada en frecuencia histórica de aparición de números
 
     def name(self):
-        # Nombre identificador de la estrategia
         return "frequency"
 
-    def predict(self, history):
-        # history = lista de sorteos anteriores
+    def predict(self, features):
 
-        numbers = []
-        # Lista donde acumulamos todos los números históricos
+        freq = features["frequency"]
 
-        for row in history:
-            # Recorre cada sorteo
-            numbers.extend(row)
-            # Agrega todos los números al listado general
-
-        counter = Counter(numbers)
-        # Cuenta cuántas veces aparece cada número
-
-        return [num for num, _ in counter.most_common(6)]
-        # Retorna los 6 números más frecuentes
+        # top 5 números más frecuentes
+        return sorted(freq, key=freq.get, reverse=True)[:5]
